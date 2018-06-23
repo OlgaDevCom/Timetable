61a75820315a7dc343a5e32dea94692ddd5e24b1
Ольга Топанова <developerdroidot@gmail.com>
2018-06-23 17:36:43 +0300

    Initial Commit

---
+++ timetable/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  timetable
+//
+//  Created by  Stanislav Topanov on 6/23/18.
+//  Copyright © 2018  Stanislav Topanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
